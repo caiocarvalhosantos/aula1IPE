1fe73a47d7efc9d4475caff4e7006d55e8e72ba4
CAIO CARVALHO SANTOS <caio.orochinho2015@gmail.com>
2025-08-30 12:42:03 -0300

    Add files via upload

---
+++ atividadeaula2.py
@@ -0,0 +1,20 @@
+print ("NOSSA PRIMEIRA CALCULADORA") #COMANDO PRINT ESCREVE UM TEXTO PREDEFINIDO NA INTERFACE OU ENTÃO O VALOR DE UMA VARIÁVEL
+
+numero01 = int (input ("INSIRA O PRIMEIRO NÚMERO: "))
+numero02 = int (input("INSIRA O SEGUNDO NÚMERO: ")) #O COMANDO INPUT COLETA UM VALOR DIGITADO PELO USUÁRIO
+#SE O TIPO FOR: (INT=NUMEROS INTEIROS) (FLOAT= NUMEROS NAO INTEIROS) (BIG INT= NUMEROS INTEIROS MAS ENORMES)
+resultadoSoma = numero01 + numero02 
+
+print(resultadoSoma)
+
+resultadoSub = numero01 - numero02 
+
+print(resultadoSub)
+
+resultadomultiplicação = numero01 * numero02
+
+print(resultadomultiplicação)
+
+resultadoDiv = numero01 / numero02
+
+print(resultadoDiv)
---
+++ exempleaula 1.py
@@ -0,0 +1,13 @@
+print("Olá Mundo")
+nome = "Caio"
+sobrenome = "Carvalho"
+idade = 19
+
+print("Seu Nome é: " + nome + " " + sobrenome + " sua idade é: " + idade)
+
+print(f"Seu nome é: {nome} {sobrenome} sua idade é: {idade}")
+
+print (f"seu nome é: {nome}")
+print (f"seu sobrenome é: {sobrenome}")
+print (f"sua idade é: {idade}")
+
